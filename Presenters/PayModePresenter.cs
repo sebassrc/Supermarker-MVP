56f6fd5bd79e31d2809666a2aad30163ea5cdc4e
Sebastian Rodriguez Camilo <segarodriguezcamilo@gamil.com>
2023-10-20 10:01:59 -0500

    pay mode presenter

--- Presenters/PayModePresenter.cs
+++ Presenters/PayModePresenter.cs
@@ -3,16 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Supermarket-mvp.Models;
-using Supermarket-mvp.Views;
+using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
 
 namespace Supermarket_mvp.Presenters
 {
     internal class PayModePresenter
     {
-        private IPayModeViews view;
+        private IPayModeView view;
         private IPayModeRepository repository;
-        private Bindinsource payModeBindinsource;
+        private BindingSource payModeBindinsource;
         private IEnumerable<PayModeModel> payModeList;
+
+        public PayModePresenter(IPayModeView view, IPayModeRepository repository)
+        {
+            this.view = view;
+            this.repository = repository;
+
+            this.view.SearchEvent += SearchPayMode;
+            this.view.AddNewEvent += AddNewPayMode;
+            this.view.EditEvent += LoadSelectPayModeToEdit;
+            this.view.DeleteEvent += DeleteSelectedPayMode;
+            this.view.SaveEvent += SavePayMode;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetPayModeListBildingSource(payModeBindinsource);
+
+            loadAllPayModeLlist();
+            this.view.show();
+        }
+
+        private void loadAllPayModeLlist()
+        {
+            payModeList = repository.GetAll();
+            payModeBindinsource.DataSource = payModeList;
+        }
+
+        private void SearchPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectPayModeToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteSelectedPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SavePayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
     }
 }
